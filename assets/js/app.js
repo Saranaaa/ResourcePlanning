0cf3160a771edfacf8e93e20a88796a9ed581bf7
saranaaa <sara.an@erp-intel.com>
2021-09-01 11:58:08 +0400

    save all

---
+++ assets/js/app.js
@@ -0,0 +1,131 @@
+let chartIsActive = true;
+$(window).scroll(() => {
+  if ($(window).scrollTop() + 500 >= $(".bars").offset().top) {
+    if (chartIsActive) {
+      $(".bars .col-4.chart1 h4").before(
+        '<canvas id="myChart1" width="400" height="400">'
+      );
+      var ctx1 = document.getElementById("myChart1");
+      var myChart = new Chart(ctx1, {
+        type: "bar",
+        data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [
+            {
+              label: "# of Votes",
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+          animations: {
+            tension: {
+              duration: 1000,
+              easing: "linear",
+              from: 1,
+              to: 0,
+              loop: true,
+            },
+          },
+        },
+      });
+      $(".bars .col-4.chart2 h4").before(
+        '<canvas id="myChart2" width="400" height="400">'
+      );
+      var ctx1 = document.getElementById("myChart2");
+      var myChart = new Chart(ctx1, {
+        type: "pie",
+        data: {
+            labels: [
+                'Red',
+                'Blue',
+                'Yellow'
+              ],
+              datasets: [{
+                label: 'My First Dataset',
+                data: [300, 50, 100],
+                backgroundColor: [
+                  'rgb(255, 99, 132)',
+                  'rgb(54, 162, 235)',
+                  'rgb(255, 205, 86)'
+                ],
+                hoverOffset: 4
+              }]
+        },
+        options: {
+          animations: {
+            tension: {
+              duration: 1000,
+              easing: "linear",
+              from: 1,
+              to: 0,
+              loop: true,
+            },
+          },
+        },
+      });
+      $(".bars .col-4.chart3 h4").before(
+        '<canvas id="myChart3" width="400" height="400">'
+      );
+      var ctx3 = document.getElementById("myChart3");
+      var myChart = new Chart(ctx3, {
+        type: "doughnut",
+        data: {
+        
+                labels: [
+                    'Red',
+                    'Blue',
+                    'Yellow'
+                  ],
+                  datasets: [{
+                    label: 'My First Dataset',
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                      'rgb(255, 99, 132)',
+                      'rgb(54, 162, 235)',
+                      'rgb(255, 205, 86)'
+                    ],
+                    hoverOffset: 4
+                  }]
+            },
+        
+        options: {
+            
+          animations: {
+            tension: {
+              duration: 1000,
+              easing: "linear",
+              from: 1,
+              to: 0,
+              loop: true,
+            },
+          },
+        },
+      });
+    }
+    if(chartIsActive) chartIsActive=false;
+  }
+  console.log($(window).scrollTop());
+});
