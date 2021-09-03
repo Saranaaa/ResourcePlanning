73a08d3a5496d53637e83293b916a8a7bda51f4b
saranaaa <sara.an@erp-intel.com>
2021-09-03 15:05:06 +0400

    save all

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,129 +1,126 @@
 let chartIsActive = true;
+let chart1 = {
+  type: "bar",
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    animations: {
+      tension: {
+        duration: 1000,
+        easing: "linear",
+        from: 1,
+        to: 0,
+        loop: true,
+      },
+    },
+  },
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+};
+let chart2 = {
+  type: "pie",
+  data: {
+    labels: ["Red", "Blue", "Yellow"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [300, 50, 100],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  },
+  options: {
+    animations: {
+      tension: {
+        duration: 1000,
+        easing: "linear",
+        from: 1,
+        to: 0,
+        loop: true,
+      },
+    },
+  },
+};
+let chart3 = {
+  type: "doughnut",
+  data: {
+    labels: ["Red", "Blue", "Yellow"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [300, 50, 100],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  },
+  options: {
+    animations: {
+      tension: {
+        duration: 1000,
+        easing: "linear",
+        from: 1,
+        to: 0,
+        loop: true,
+      },
+    },
+  },
+};
+
+
+function loadChart(chartBox, chartId, chart) {
+  let title = $(`.bars ${chartBox}`).attr("title");
+  $(`.bars ${chartBox}`).append(
+    `<canvas id="${chartId}" width="400" height="400">`
+  );
+  $(`.bars ${chartBox} canvas`).after(`<h4>${title}</h4>`);
+  var ctx = document.getElementById(chartId);
+  new Chart(ctx, {
+    type: chart.type,
+    data: chart.data,
+    options: chart.options,
+  });
+}
 
 $(window).scroll(() => {
   if ($(window).scrollTop() + 500 >= $(".bars").offset().top) {
     if (chartIsActive) {
-      let title = $(".bars .chart1").attr("title");
-      $(".bars .chart1").append(
-        '<canvas id="myChart1" width="400" height="400">'
-      );
-      $(".bars .chart1 canvas").after(`<h4>${title}</h4>`);
-      var ctx1 = document.getElementById("myChart1");
-      var myChart = new Chart(ctx1, {
-        type: "bar",
-        data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-          datasets: [
-            {
-              label: "# of Votes",
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)",
-              ],
-              borderWidth: 1,
-            },
-          ],
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
-          animations: {
-            tension: {
-              duration: 1000,
-              easing: "linear",
-              from: 1,
-              to: 0,
-              loop: true,
-            },
-          },
-        },
-      });
-
-      title = $(".bars .chart2").attr("title");
-      $(".bars .chart2").append('<canvas id="myChart2" width="400" ght="400">');
-      $(".bars .chart2 canvas").after(`<h4>${title}</h4>`);
-
-      var ctx2 = document.getElementById("myChart2");
-      var myChart = new Chart(ctx2, {
-        type: "pie",
-        data: {
-          labels: ["Red", "Blue", "Yellow"],
-          datasets: [
-            {
-              label: "My First Dataset",
-              data: [300, 50, 100],
-              backgroundColor: [
-                "rgb(255, 99, 132)",
-                "rgb(54, 162, 235)",
-                "rgb(255, 205, 86)",
-              ],
-              hoverOffset: 4,
-            },
-          ],
-        },
-        options: {
-          animations: {
-            tension: {
-              duration: 1000,
-              easing: "linear",
-              from: 1,
-              to: 0,
-              loop: true,
-            },
-          },
-        },
-      });
-
-      title = $(".bars .chart3").attr("title");
-      $(".bars .chart3").append('<canvas id="myChart3" width="400" ght="400">');
-      $(".bars .chart3 canvas").after(`<h4>${title}</h4>`);
-      var ctx3 = document.getElementById("myChart3");
-      var myChart = new Chart(ctx3, {
-        type: "doughnut",
-        data: {
-          labels: ["Red", "Blue", "Yellow"],
-          datasets: [
-            {
-              label: "My First Dataset",
-              data: [300, 50, 100],
-              backgroundColor: [
-                "rgb(255, 99, 132)",
-                "rgb(54, 162, 235)",
-                "rgb(255, 205, 86)",
-              ],
-              hoverOffset: 4,
-            },
-          ],
-        },
-
-        options: {
-          animations: {
-            tension: {
-              duration: 1000,
-              easing: "linear",
-              from: 1,
-              to: 0,
-              loop: true,
-            },
-          },
-        },
-      });
       $(".owl-carousel").owlCarousel({
         loop: true,
         autoplay: true,
@@ -134,32 +131,67 @@ $(window).scroll(() => {
             items: 1,
           },
         },
+        onInitialized: () => {
+          loadChart(".owl-item.active .chart1", "myChart1", chart1);
+        },
+        onTranslated: (e) => {
+          console.log(e.item.index);
+          if (e.item.index == 3) {
+            $(".bars .chart2").empty();
+            loadChart(".owl-item.active .chart2", "myChart2", chart2);
+          }
+          if (e.item.index == 4) {
+            $(".bars .chart3").empty();
+            loadChart(".owl-item.active .chart3", "myChart3", chart3);
+          }
+
+          if (e.item.index == 2 || e.item.index == 5) {
+            $(".bars .chart1").empty();
+            loadChart(".owl-item.active .chart1", "myChart1", chart1, "bar");
+
+          }
+        },
       });
     }
     if (chartIsActive) chartIsActive = false;
   }
 
-  if($(window).scrollTop() + 80 <= $("#about").offset().top && $(window).scrollTop()+90>=  $("#home").offset().top ){
+  
+  if (
+    $(window).scrollTop() + 80 <= $("#about").offset().top &&
+    $(window).scrollTop() + 90 >= $("#home").offset().top
+  ) {
     $(".nav_scroll a.active").removeClass("active");
     $(".nav_scroll a[href='#home']").addClass("active");
   }
-  if($(window).scrollTop() + 80 <= $("#service").offset().top && $(window).scrollTop()+90>=  $("#about").offset().top){
+  if (
+    $(window).scrollTop() + 80 <= $("#service").offset().top &&
+    $(window).scrollTop() + 90 >= $("#about").offset().top
+  ) {
     $(".nav_scroll a.active").removeClass("active");
     $(".nav_scroll a[href='#about']").addClass("active");
   }
-  if($(window).scrollTop() + 80 <= $("#faq").offset().top && $(window).scrollTop()+90>=  $("#service").offset().top){
+  if (
+    $(window).scrollTop() + 80 <= $("#faq").offset().top &&
+    $(window).scrollTop() + 90 >= $("#service").offset().top
+  ) {
     $(".nav_scroll a.active").removeClass("active");
     $(".nav_scroll a[href='#service']").addClass("active");
   }
-  if($(window).scrollTop() + 80 <= $("#contact").offset().top && $(window).scrollTop()+90>=  $("#faq").offset().top){
+  if (
+    $(window).scrollTop() + 80 <= $("#contact").offset().top &&
+    $(window).scrollTop() + 90 >= $("#faq").offset().top
+  ) {
     $(".nav_scroll a.active").removeClass("active");
     $(".nav_scroll a[href='#faq']").addClass("active");
   }
-  if($(window).scrollTop() + 80 <= $(".footer-middle").offset().top && $(window).scrollTop()+90>=  $("#contact").offset().top){
+  if (
+    $(window).scrollTop() + 80 <= $(".footer-middle").offset().top &&
+    $(window).scrollTop() + 90 >= $("#contact").offset().top
+  ) {
     $(".nav_scroll a.active").removeClass("active");
     $(".nav_scroll a[href='#contact']").addClass("active");
   }
-  
 });
 
 $(() => {
